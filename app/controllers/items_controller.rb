e1850fc1e09c710d6eab9ba72c34c834dcc84ccb
Graham <blundell.gp@gmail.com>
2024-04-15 16:56:05 -0700

    added final functionality and tests

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,26 @@ class ItemsController < ApplicationController
         end
     end
 
+    def update
+        item = Item.find(params[:id])
+        item.update(item_params)
+        if item.valid?
+            render json: item
+        else
+            render json: item.errors, status: 422
+        end
+    end
+
+    def destroy
+        item = Item.find(params[:id])
+        item.destroy
+        if item.valid?
+            render json: item
+        else
+            render json: item.errors, status: 422
+        end
+    end
+
     private
     def item_params
         params.require(:item).permit(:title, :price, :contact, :location, :condition, :description, :image, :user_id)
