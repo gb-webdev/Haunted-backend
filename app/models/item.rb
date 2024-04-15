cdee1050b0ea3f5cdff8c87cedd9b52e5e12013a
Graham <blundell.gp@gmail.com>
2024-04-15 15:24:32 -0700

    added model specs and validated

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
     belongs_to :user
+    validates :title, presence: true
+    validates :price, presence: true
+    validates :contact, presence: true
+    validates :location, presence: true
+    validates :condition, presence: true
+    validates :description, presence: true
+    validates :image, presence: true
 end
